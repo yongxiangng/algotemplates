c5b1859ea7a93151aace40309d79818b100d288d
yongxiangng <yongxiangng@gmail.com>
2022-03-14 00:31:42 +0800

    Add insane mat mult cache

---
+++ misc/mat_mult_steroids.cpp
@@ -0,0 +1,9 @@
+int main() {
+    for (int i = 0; i < n; ++i) {
+        for (int k = 0; k < n; ++k) {
+            for (int j = 0; j < n; ++j) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
